02e656dcc35938427c3945b82688e4a9e4887cb3
Abeo Anthony, A <anthonyabeo@gmail.com>
2019-10-19 15:05:29 +0000

    set up switch table for parsing constant pool.

--- source/app.d
+++ source/app.d
@@ -1,4 +1,7 @@
-import std.stdio, std.file;
+import std.stdio, std.file, std.variant;
+
+
+alias CP_INFO = Algebraic!(MethodRef);
 
 void main()
 {
@@ -9,7 +12,7 @@ void main()
 	auto min_version = BE16(buffer[4 .. 6]);
 	auto maj_version = BE16(buffer[6 .. 8]);
 	auto const_pool_cnt = BE16(buffer[8 .. 10]);
-	cp_info[] const_pool = build_const_pool(buffer, 10);
+	build_const_pool(buffer[10 .. const_pool_cnt + 10]);
 
 	writefln("%x", magic);
 	writefln("%x", min_version);
@@ -17,7 +20,7 @@ void main()
 	writefln("%x", const_pool_cnt);
 }
 
-auto BE32(ubyte[] data) 
+auto BE32(const ubyte[] data) 
 {
 	return data[0] << 24 |
 	       data[1] << 16 |
@@ -25,13 +28,164 @@ auto BE32(ubyte[] data)
 		   data[3];
 }
 
-auto BE16(ubyte[] data) 
+auto BE16(const ubyte[] data) 
 {
-	return data[0] << 8  |
-		   data[1];
+	return data[0] << 8  | 
+	       data[1];
 }
 
-cp_info[] build_const_pool(ubyte[] buffer, size_t start_index) 
+void build_const_pool(const ubyte[] buffer) 
 {
-	
+	CP_INFO[] pool = new CP_INFO[buffer.length];
+
+	size_t i = 0, next_index = 1;
+	while(i < 50)
+	{
+		ubyte tag = buffer[i];
+		final switch(tag) 
+		{
+			case Constant.Methodref:
+				writeln("methodoligical");
+				size_t class_index = BE16(buffer[i+1 .. i+3]);
+				size_t name_type_index = BE16(buffer[i+3 .. i+5]);
+
+				auto method_ref = MethodRef(tag, class_index, name_type_index);
+				pool[next_index] = Algebraic!(MethodRef)(method_ref);
+
+				next_index += 1;
+				i += 5;
+
+				// writeln(method_ref);
+				// writeln(pool);
+				break;
+
+			case Constant.Fieldref:
+				writeln("field you best");
+
+				next_index += 1;
+				i += 5;
+
+				break;
+			
+			case Constant.InterfaceMethodref:
+				writeln("interfacing methods");
+				
+				next_index += 1;
+				i += 5;
+
+				break;
+
+			case Constant.Integer:
+				writeln("integral");
+				
+				next_index += 1;
+				i += 5;
+
+				break;
+
+			case Constant.Float:
+				writeln("floating away");
+				
+				next_index += 1;
+				i += 5;
+
+				break;
+
+			case Constant.Long:
+				writeln("longing for love");
+
+				next_index += 1;
+				i += 9;
+				break;
+
+			case Constant.Double:
+				writeln("doubling down");
+
+				next_index += 1;
+				i += 9;
+				break;
+
+			case Constant.Klass:
+				writeln("classless cassidy");
+
+				next_index += 1;
+				i += 3;
+
+				break;
+			
+			case Constant.String:
+				writeln("stringifying");
+
+				next_index += 1;
+				i += 3;
+
+				break;
+
+			case Constant.NameAndType:
+				writeln("Name Type");
+
+				next_index += 1;
+				i += 5;
+				break;
+
+			case Constant.Utf8:
+				next_index += 1;
+
+				break;
+		}
+		// break;
+	}
+}
+
+enum Constant : ubyte 
+{
+	Klass = 7,
+	Fieldref = 9,
+	Methodref = 10,
+	InterfaceMethodref = 11,
+	String = 8,
+	Integer = 3,
+	Float = 4,
+	Long = 5,
+	Double = 6,
+	NameAndType = 12,
+	Utf8 = 1
+}
+
+struct MethodRef 
+{
+	ubyte tag;
+	size_t class_index;
+	size_t name_type_index;
+
+	this(ubyte tag, size_t class_index, size_t name_type_index) {
+		this.tag = tag;
+		this.class_index = class_index;
+		this.name_type_index = name_type_index;
+	}
+}
+
+struct FieldRef
+{
+	ubyte tag;
+	size_t class_index;
+	size_t name_type_index;
+
+	this(ubyte tag, size_t class_index, size_t name_type_index) {
+		this.tag = tag;
+		this.class_index = class_index;
+		this.name_type_index = name_type_index;
+	}
+}
+
+struct Class 
+{
+	ubyte tag;
+	size_t name_index;
+}
+
+struct Float
+{
+	ubyte tag;
+	float bytes;
 }
