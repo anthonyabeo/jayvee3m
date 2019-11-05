81c2c0014e190c01befd520cad8fda94bdb2d888
Abeo Anthony, A <anthonyabeo@gmail.com>
2019-11-05 17:54:29 +0000

    add stack frame and javaStack to perThread memory.

--- source/jvm/memory.d
+++ source/jvm/memory.d
@@ -1,4 +1,7 @@
+import std.typecons : Tuple;
+
 import class_loader.class_file : ClassFile;
+import constants : CP_INFO;
 
 struct Shared
 {
@@ -11,5 +14,34 @@ struct PerThread
 {
     size_t pc;
     size_t sp;
-    // java stack
+    StackFrame[] javaStack;
+}
+
+struct StackFrame
+{
+    // local variables array
+    int[] locals;
+
+    // operand stack
+    int[] operandStack;
+
+    // reference to the current constant pool
+    //CP_INFO[] constPool;
+
+    // exception table
+    //Tuple!(size_t, size_t, size_t, size_t) exceptionTable;
+
+    // instructions
+    const(ubyte[]) code;
+
+    @safe pure nothrow
+    this(size_t locals_size, size_t stack_size, const(ubyte[]) code)
+    {
+        this.locals = new int[locals_size];
+        this.operandStack = new int[stack_size];
+        //this.constPool = pool;
+        //this.exceptionTable = excp_table;
+        this.code = code;
+    }
+    
 }
