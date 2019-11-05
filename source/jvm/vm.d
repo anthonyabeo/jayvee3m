0db4cdf0afff0184f390008242d99a5ec15527fc
Abeo Anthony, A <anthonyabeo@gmail.com>
2019-11-05 17:56:59 +0000

    begin to execute main class constructor.

--- source/jvm/vm.d
+++ source/jvm/vm.d
@@ -1,8 +1,10 @@
 import std.stdio;
 
-import memory: Shared, PerThread;
-import class_loader.class_file: ClassFile;
-
+import memory : Shared, PerThread, StackFrame;
+import class_loader.class_file : ClassFile;
+import constants : UTF8;
+import attributes : ATTR_INFO, Code, get_attribute;
+import instructions: Opcode;
 
 struct JVM
 {
@@ -17,13 +19,64 @@ struct JVM
         this.perThread = newPerThread();
     }
 
-    static PerThread newPerThread() {
-        return PerThread(0, 0);
+    static PerThread newPerThread()
+    {
+        StackFrame[] frame;
+        return PerThread(0, 0, frame);
     }
 
     void start()
     {
         auto mainClassFile = this.sharedMemory.method_area[this.mainClass];
-        writeln(mainClassFile.m_info);
+
+        // Call Main's constructor
+        foreach (mthd; mainClassFile.m_info)
+        {
+            const name = *mainClassFile.constant_pool[mthd.name_index].peek!(UTF8);
+            const descrptor = *mainClassFile.constant_pool[mthd.descriptor_index].peek!(UTF8);
+
+            bool isMainConstructor = mthd.access_flags == 0x01 &&
+                                     name.value == cast(ubyte[]) "<init>" &&
+                                     descrptor.value == cast(ubyte[]) "()V";
+
+            if (isMainConstructor)
+            {
+                foreach (attribute; mthd.attributes)
+                {
+                    auto c = get_attribute(attribute);
+                    auto frame = StackFrame(c.max_locals, c.max_stack, c.code);
+
+                    this.perThread.javaStack ~= frame;
+                    execute();
+                }
+            }
+        }
+    }
+
+    void execute()
+    {
+        const curStack = this.perThread.javaStack[0];
+        writeln(curStack);
+
+        while(this.perThread.pc < curStack.code.length)
+        {
+            immutable opcode = curStack.code[this.perThread.pc];
+            final switch(opcode)
+            {
+                case Opcode.aload_0:
+                    writeln("executing: aload_0");
+                    this.perThread.pc += 1;
+                    break;
+                case Opcode.invokespecial:
+                    writeln("executing: invokespecial");
+                    this.perThread.pc += 3;
+                    break;
+                case Opcode.riturn:
+                    writeln("executing: return");
+                    this.perThread.pc += 1;
+                    break;
+            }
+        }
+
     }
 }
